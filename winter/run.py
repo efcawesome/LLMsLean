b7ab8c5dcd2a137f584912bd6a8400327d15d61a
abladek <drew.bladek@gmail.com>
2026-01-06 12:38:53 -0800

    Added Basic Description

--- winter/run.py
+++ winter/run.py
@@ -2,7 +2,7 @@ from generate import *
 from verify import *
 
 
-load_dotenv("../../.env")
+load_dotenv("../.env")
 
 #generate("../data/winter/test3B.jsonl", "../data/winter/test3C.jsonl", "us.anthropic.claude-sonnet-4-5-20250929-v1:0", 0, True)
 #verify("../data/winter/test3C.jsonl", "../data/winter/test3C.jsonl")
